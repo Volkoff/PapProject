21e95f3ff566d0c3816a6098ba6dcbbfc7d93947
Anton Volkov <antonmivo@gmail.com>
2021-05-15 14:40:54 +0200

    Pause menu

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Movement : MonoBehaviour
 {
     public float camRotY;
     public float speed = 0.08f;
     public float jumpForce = 0.5f;
     public float sensitivity = 2;
+    int check;
 
     private Rigidbody rb;
 
@@ -24,12 +25,30 @@ public class Movement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
+    string PrevScene = "Level1";
+    void SceneManage(int check){
+        if(check == 1){
+            SceneManager.LoadScene("Escape screen");
+        }
+        if(check == 2){
+            SceneManager.LoadScene(PrevScene);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         camRotY += sensitivity * Input.GetAxis("Mouse X");
         transform.localEulerAngles = new Vector3(0, camRotY, 0);
 
+        if(Input.GetKeyDown("escape") && SceneManager.GetActiveScene().name == "Level1"){
+            check = 1;
+            PrevScene = SceneManager.GetActiveScene().name;
+            SceneManage(check);
+        }
+        if(Input.GetKeyDown("escape") && SceneManager.GetActiveScene().name == "Escape screen"){
+            check = 2;
+            SceneManage(check);
+        }
         if (Input.GetAxis("Horizontal") != 0)
         {
             gameObject.transform.Translate(speed * Input.GetAxis("Horizontal"), 0, 0);
